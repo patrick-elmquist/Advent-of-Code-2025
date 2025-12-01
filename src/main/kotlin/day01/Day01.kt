c5ff26a413d1424a9efdb4b74cdf928373797a17
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2025-12-01 08:05:52 +0100

    Day 1 done

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -2,25 +2,50 @@ package day01
 
 import common.day
 
-// answer #1:
-// answer #2:
+// answer #1: 1089
+// answer #2: 6530
 
 fun main() {
     day(n = 1) {
         part1 { input ->
-
+            var pos = 50
+            input.lines.count { line ->
+                val (direction, distance) = parseDirectionAndDistance(line)
+                pos += direction * distance
+                pos %= 100
+                pos == 0
+            }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 1089
+            run test 1 expect 3
         }
 
         part2 { input ->
-
+            var pos = 50
+            input.lines.sumOf { line ->
+                val (direction, distance) = parseDirectionAndDistance(line)
+                (1..distance).count {
+                    pos += direction
+                    pos %= 100
+                    pos == 0
+                }
+            }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 6530
+            run test 1 expect 6
         }
     }
 }
+
+private fun parseDirectionAndDistance(line: String): Pair<Int, Int> =
+    Pair(
+        line.first().let {
+            when (it) {
+                'L' -> -1
+                else -> 1
+            }
+        },
+        line.drop(1).toInt(),
+    )
