8a115ade0c63a5abd3a421330e9ac447dbf68a59
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2025-12-06 12:07:39 +0100

    Day 6 cleanup

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -5,26 +5,26 @@ import common.day
 // answer #1: 6295830249262
 // answer #2: 9194682052782
 
+private val whitespaceRegex = "\\s+".toRegex()
+
 fun main() {
     day(n = 6) {
         part1 { input ->
-            val operators = input.lines.last().trim().split("\\s+".toRegex())
+            val trimmed = input.lines.map { it.trim().split(whitespaceRegex).map { it.trim() } }
+            val operators = trimmed.last()
             val lists = List(operators.size) { mutableListOf<Long>() }
-            input.lines.dropLast(1)
+            trimmed.dropLast(1)
                 .forEach { line ->
-                    line.trim()
-                        .split("\\s+".toRegex())
-                        .forEachIndexed { i, number ->
-                            lists[i].add(number.toLong())
-                        }
+                    line.forEachIndexed { i, number ->
+                        lists[i].add(number.toLong())
+                    }
                 }
 
-            lists.mapIndexed { index, numbers ->
-                when (operators[index]) {
-                    "*" -> numbers.reduce(Long::times)
-                    else -> numbers.reduce(Long::plus)
+            lists
+                .mapIndexed { index, numbers ->
+                    numbers.reduce(operators[index].first())
                 }
-            }.sum()
+                .sum()
         }
         verify {
             expect result 6295830249262L
@@ -33,16 +33,12 @@ fun main() {
 
         part2 { input ->
             val lines = input.lines.map { "$it " }
-            val columnIndices = lines.first().indices
             var operator: Char? = null
             val numbers = mutableListOf<Long>()
-            columnIndices.sumOf { index ->
+            lines.first().indices.sumOf { index ->
                 val column = lines.map { it[index] }
                 if (column.all(Char::isWhitespace)) {
-                    when (operator!!) {
-                        '*' -> numbers.reduce(Long::times)
-                        else -> numbers.reduce(Long::plus)
-                    }
+                    numbers.reduce(operator!!)
                 } else {
                     if (column.last() in setOf('*', '+')) {
                         operator = column.last()
@@ -58,4 +54,11 @@ fun main() {
             run test 1 expect 3263827L
         }
     }
+
 }
+
+private fun List<Long>.reduce(operator: Char): Long =
+    when (operator) {
+        '*' -> reduce(Long::times)
+        else -> reduce(Long::plus)
+    }
