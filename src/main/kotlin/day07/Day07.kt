4ac4ac71aa0cc47bf63fb850fa7a46ba3ec897b5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2025-12-07 16:56:45 +0100

    Day 7 cleanup

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -8,24 +8,27 @@ import common.util.Point
 // answer #1: 1681
 // answer #2: 422102272495018
 
+private val offsets = listOf(-1, 1)
+
 fun main() {
     day(n = 7) {
         part1 { input ->
             val splitters = parseSplitters(input)
             val queue = mutableListOf(splitters.minBy { it.y })
 
-            val visitedSplitters = mutableSetOf<Point>()
-            val offsets = listOf(-1, 1)
+            val visited = mutableSetOf<Point>()
             while (queue.isNotEmpty()) {
                 val splitter = queue.removeFirst()
 
-                if (splitter in visitedSplitters) continue
-                visitedSplitters += splitter
+                if (splitter in visited) continue
+                visited += splitter
 
-                queue += offsets.mapNotNull { splitters.findSplitterBelow(splitter, it) }
+                queue += offsets.mapNotNull { offset ->
+                    splitters.findSplitterBelow(splitter, offset)
+                }
             }
 
-            visitedSplitters.size
+            visited.size
         }
 
         verify {
@@ -48,41 +51,31 @@ fun main() {
     }
 }
 
-private fun parseSplitters(input: Input): List<Point> =
-    input.grid
-        .filterValues { it == '^' }
-        .keys
-        .toList()
-
 private fun countTimelinesRecursively(
     splitter: Point,
     splitters: List<Point>,
     cache: MutableMap<Point, Long> = mutableMapOf(),
 ): Long = cache.getOrPut(splitter) {
-    val nextLeftSplitter = splitters.findSplitterBelow(splitter, offset = -1)
-    val leftTimelines = if (nextLeftSplitter == null) {
-        1L
-    } else {
-        countTimelinesRecursively(
-            splitter = nextLeftSplitter,
-            splitters = splitters,
-            cache = cache,
-        )
-    }
-
-    val nextRightSplitter = splitters.findSplitterBelow(splitter, offset = 1)
-    val rightTimelines = if (nextRightSplitter == null) {
-        1L
-    } else {
-        countTimelinesRecursively(
-            splitter = nextRightSplitter,
-            splitters = splitters,
-            cache = cache,
-        )
-    }
-
-    leftTimelines + rightTimelines
+    offsets
+        .map { offset -> splitters.findSplitterBelow(splitter, offset) }
+        .sumOf { nextSplitter ->
+            if (nextSplitter == null) {
+                1L
+            } else {
+                countTimelinesRecursively(
+                    splitter = nextSplitter,
+                    splitters = splitters,
+                    cache = cache,
+                )
+            }
+        }
 }
 
-private fun List<Point>.findSplitterBelow(p: Point, offset: Int): Point? =
-    firstOrNull { it.x == p.x + offset && it.y > p.y }
+private fun List<Point>.findSplitterBelow(splitter: Point, offset: Int): Point? =
+    firstOrNull { it.x == splitter.x + offset && it.y > splitter.y }
+
+private fun parseSplitters(input: Input): List<Point> =
+    input.grid
+        .filterValues { it == '^' }
+        .keys
+        .toList()
