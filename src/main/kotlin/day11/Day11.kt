d2001b22176bf8e8643a00d7178c05d1aef14bfb
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2025-12-11 08:25:28 +0100

    Day 11 Part 2 done

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -2,8 +2,8 @@ package day11
 
 import common.day
 
-// answer #1:
-// answer #2:
+// answer #1: 772
+// answer #2: 423227545768872
 
 fun main() {
     day(n = 11) {
@@ -13,38 +13,45 @@ fun main() {
                 val (start, ends) = line.split(": ").let { (a, b) -> a to b.split(" ") }
                 map[start] = ends
             }
-            println(map)
 
-            val me = map["you"]
-            println(me)
-            findPaths(start = "you", map = map)
+            findPaths(start = "you", target = "out", map = map)
         }
         verify {
-            expect result null
+            expect result 772L
             run test 1 expect 5L
         }
 
         part2 { input ->
-
+            val map = mutableMapOf<String, List<String>>()
+            input.lines.forEach { line ->
+                val (start, ends) = line.split(": ").let { (a, b) -> a to b.split(" ") }
+                map[start] = ends
+            }
+            // looking at the graph, the order is svr -> fft -> dac -> out
+            val pathsFromSvrToFft = findPaths(start = "svr", target = "fft", map = map)
+            val pathsFromFftToDac = findPaths(start = "fft", target = "dac", map = map)
+            val pathsFromDacToOut = findPaths(start = "dac", target = "out", map = map)
+            pathsFromSvrToFft * pathsFromFftToDac * pathsFromDacToOut
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 423227545768872L
+            run test 2 expect 2L
         }
     }
 }
 
 private fun findPaths(
     start: String,
+    target: String,
     map: Map<String, List<String>>,
     cache: MutableMap<String, Long> = mutableMapOf(),
 ): Long {
     return cache.getOrPut(start) {
         map.getValue(start).sumOf { end ->
-            if (end == "out") {
+            if (end == target) {
                 1L
             } else {
-                findPaths(end, map, cache)
+                findPaths(end, target, map, cache)
             }
         }
     }
